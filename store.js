e24d5b9b0dd22ed9d9aadf839845808369f98c60
ausin107 <nguyenphuqui396@gmail.com>
2022-07-27 22:21:50 +0700

    add firebase tailwindcss redux toolkit

---
+++ store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit'
+import authReducer from './component/auth/authSlice'
+export const store = configureStore({
+  reducer: {
+    //gán cho auth bằng initialState thuộc authReducer
+    //-> truy cập bằng useSelector()
+    //-> state.auth.isAuth or state.auth.authKey
+    auth: authReducer,
+  },
+})
